34bd1ae0403d7979ad35dbe5f5f2f5b5d017ae5b
dev <romansyah26588@gmail.com>
2025-10-22 20:22:44 +0700

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,4 @@
 import { OpenNextConfig } from '@opennextjs/cloudflare';
-// Tidak perlu import types jika menggunakan .js
-@type {import('@opennextjs/cloudflare').OpenNextConfig}
-const config = {
-  // Tambahkan konfigurasi spesifik di sini jika diperlukan.
-  // Misalnya, jika Anda memiliki aset statis yang perlu diabaikan atau disalin secara khusus.
-};
-
+/** @type {import('@opennextjs/cloudflare').OpenNextConfig} */
+const config = {};
 module.exports = config;
