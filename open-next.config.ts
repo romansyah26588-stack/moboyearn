b03d2b091301bf2b2737b6e1602b608c5299ccc9
dev <romansyah26588@gmail.com>
2025-10-23 05:32:01 +0700

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,12 @@
 // open-next.config.ts
 
 /** @type {import('@opennextjs/cloudflare').OpenNextConfig} */
-const config = {};
+const config = {
+  // Masukkan objek 'default' yang berisi konfigurasi utama Next.js Anda
+  default: {
+    // Override minimal yang dibutuhkan untuk menghindari error 'cannot be empty'
+    override: {}, 
+  },
+};
 
 export default config;
