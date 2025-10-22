8120b4276faf8e219990dcf3363a1722449f3c5d
dev <romansyah26588@gmail.com>
2025-10-22 19:33:08 +0700

    Create open-next.config.ts

---
+++ open-next.config.ts
@@ -0,0 +1,8 @@
+import { OpenNextConfig } from '@opennextjs/cloudflare';
+
+const config: OpenNextConfig = {
+  // Hanya jika Anda perlu mengkonfigurasi lebih lanjut
+  // Jika tidak, biarkan objek konfigurasi kosong
+};
+
+export default config;
