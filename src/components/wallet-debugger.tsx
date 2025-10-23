8ee6615f7970a212e72fbc26dbf7e314fd7114db
dev <romansyah26588@gmail.com>
2025-10-23 13:00:41 +0700

    Update wallet-debugger.tsx

--- src/components/wallet-debugger.tsx
+++ src/components/wallet-debugger.tsx
@@ -14,11 +14,12 @@ export function WalletDebugger() {
     if (typeof window !== 'undefined') {
       console.log('Phantom installed:', !!window.solana?.isPhantom);
       
-      // PERBAIKAN: Ganti pengecekan isConnected dengan publicKey
+      // PERBAIKAN: Untuk Phantom, gunakan publicKey untuk mengecek koneksi
       console.log('Phantom connected:', !!window.solana?.publicKey);
       console.log('Phantom public key:', window.solana?.publicKey?.toString());
 
       console.log('Solflare installed:', !!window.solflare);
+      // Solflare mungkin memiliki properti isConnected, jadi bisa dibiarkan
       console.log('Solflare connected:', !!window.solflare?.isConnected);
       console.log('Solflare public key:', window.solflare?.publicKey?.toString());
     }
