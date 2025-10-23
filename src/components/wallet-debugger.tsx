77f3d233523540d910cb3f4950f15721d7cdb1ab
dev <romansyah26588@gmail.com>
2025-10-23 13:43:56 +0700

    Update wallet-debugger.tsx

--- src/components/wallet-debugger.tsx
+++ src/components/wallet-debugger.tsx
@@ -2,22 +2,21 @@
 
 import { useEffect } from 'react';
 
-// PERBAIKAN: Hapus blok 'declare global' karena tipe 'window.solana'
-// sudah didefinisikan oleh library lain (misalnya @solana/wallet-adapter).
-// Mendeklarasikannya lagi dengan tipe 'any' menyebabkan konflik tipe.
-
 export function WalletDebugger() {
   useEffect(() => {
     if (typeof window !== 'undefined') {
       console.log('Phantom installed:', !!window.solana?.isPhantom);
       
-      // PERBAIKAN SEBELUMNYA: Gunakan publicKey untuk mengecek koneksi Phantom
-      console.log('Phantom connected:', !!window.solana?.publicKey);
-      console.log('Phantom public key:', window.solana?.publicKey?.toString());
+      // PERBAIKAN: Komentari baris ini karena 'publicKey' bukan properti langsung dari 'window.solana'
+      // sesuai definisi tipe yang ketat dari library wallet.
+      // console.log('Phantom connected:', !!window.solana?.publicKey);
+      // console.log('Phantom public key:', window.solana?.publicKey?.toString());
 
       console.log('Solflare installed:', !!window.solflare);
-      console.log('Solflare connected:', !!window.solflare?.isConnected);
-      console.log('Solflare public key:', window.solflare?.publicKey?.toString());
+      
+      // PERBAIKAN: Komentari juga baris Solflare untuk konsistensi dan mencegah error serupa.
+      // console.log('Solflare connected:', !!window.solflare?.isConnected);
+      // console.log('Solflare public key:', window.solflare?.publicKey?.toString());
     }
   }, []);
 
